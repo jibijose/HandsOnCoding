7a5586042c1e55ed9a3178580b438831eece2492
Jibi Jose <jibijose@yahoo.com>
2018-11-15 19:09:30 +0530

    modules jigsaw added

---
+++ jdk11playground/Greetings/src/main/java/com/greetings/Application.java
@@ -0,0 +1,21 @@
+package com.greetings;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.astro.World;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class).web(false).run(args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Greetings!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + World.name());
+  }
+
+}
