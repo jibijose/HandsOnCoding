498e4ade0a5cf1884be8d89755d394ae07901aeb
Jibi Jose <jibijose@yahoo.com>
2018-11-21 15:14:20 +0530

    eight queens

--- DataStructuresAlgorithms/src/main/java/com/test/dsalg/chess/EightQueens.java
+++ DataStructuresAlgorithms/src/main/java/com/test/dsalg/chess/EightQueens.java
@@ -5,8 +5,11 @@ public class EightQueens {
     public static int BOARDSIZE = 8;
     private int numOfSolutions = 0;
 
-    int[][] attackedStart = new int[BOARDSIZE][BOARDSIZE];
     int[][] boardStart = new int[BOARDSIZE][BOARDSIZE];
+    int[] boardStateX = new int[BOARDSIZE];
+    int[] boardStateY = new int[BOARDSIZE];
+    int[] boardStateXR = new int[2 * BOARDSIZE - 1];
+    int[] boardStateXL = new int[2 * BOARDSIZE - 1];
 
     public static void main(String[] args) {
         EightQueens eightQueens = new EightQueens();
@@ -14,32 +17,42 @@ public class EightQueens {
     }
 
     private void findSolutions() {
-        placeQueen(0, attackedStart);
+        placeQueen(0);
     }
 
-    private void placeQueen(int i, int[][] attacked) {
+    private void placeQueen(int i) {
         if (i == BOARDSIZE) {
             numOfSolutions++;
             System.out.println("******************* SOLUTION " + numOfSolutions + " ******************************");
-            printBoard();
+            //printBoard();
             return;
         }
 
         for (int j = 0; j < BOARDSIZE; j++) {
-            //System.out.println("CHECKING [" + i + "," + j + "]");
-            //printBoard();
-            if (checkQueenAttacksEachOther(i, j)) {
-                //System.out.println("QUEENS ATTACK EACH OTHER!!!");
+            if (checkQueenAttacksEachOtherOptimized(i, j)) {
                 continue;
             }
-            int[][] newAttacked = updateAttacked(i, j, attacked);
-            boardStart[i][j] = 1;
-            //System.out.println("PLACED QUEEN [" + i + "," + j + "]");
-            //printAttack(newAttacked);
-            placeQueen(i + 1, newAttacked);
-            boardStart[i][j] = 0;
-            //System.out.println("REMOVED QUEEN [" + i + "," + j + "]");
+            prePlaceQueen(i, j);
+            placeQueen(i + 1);
+            postPlaceQueen(i, j);
+
         }
+    }
+
+    private void prePlaceQueen(int i, int j) {
+        boardStart[i][j] = 1;
+        boardStateX[i] = 1;
+        boardStateY[j] = 1;
+        boardStateXR[i - j + BOARDSIZE - 1] = 1;
+        boardStateXL[i + j] = 1;
+    }
+
+    private void postPlaceQueen(int i, int j) {
+        boardStart[i][j] = 0;
+        boardStateX[i] = 0;
+        boardStateY[j] = 0;
+        boardStateXR[i - j + BOARDSIZE - 1] = 0;
+        boardStateXL[i + j] = 0;
     }
 
     private void printBoard() {
@@ -52,83 +65,20 @@ public class EightQueens {
         }
     }
 
-    private void printAttack(int[][] attacked) {
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        for (int i = 0; i < BOARDSIZE; i++) {
-            for (int j = 0; j < BOARDSIZE; j++) {
-                System.out.print(attacked[i][j] + " ");
-            }
-            System.out.println();
+    private boolean checkQueenAttacksEachOtherOptimized(int iQueen, int jQueen) {
+        if (boardStateX[iQueen] == 1) {
+            return true;
         }
-    }
-
-    private boolean checkQueenAttacksEachOther(int iQueen, int jQueen) {
-        for (int i = 0; i < BOARDSIZE; i++) {
-            if (boardStart[i][jQueen] == 1) {
-                return true;
-            }
+        if (boardStateY[jQueen] == 1) {
+            return true;
         }
-        for (int j = 0; j < BOARDSIZE; j++) {
-            if (boardStart[iQueen][j] == 1) {
-                return true;
-            }
+        if (boardStateXR[iQueen - jQueen + BOARDSIZE - 1] == 1) {
+            return true;
         }
-        for (int index = 0; index < BOARDSIZE; index++) {
-            if (iQueen + index < BOARDSIZE && jQueen + index < BOARDSIZE) {
-                if (boardStart[iQueen + index][jQueen + index] == 1) {
-                    return true;
-                }
-            }
-            if (iQueen - index >= 0 && jQueen + index < BOARDSIZE) {
-                if (boardStart[iQueen - index][jQueen + index] == 1) {
-                    return true;
-                }
-            }
-            if (iQueen + index < BOARDSIZE && jQueen - index >= 0) {
-                if (boardStart[iQueen + index][jQueen - index] == 1) {
-                    return true;
-                }
-            }
-            if (iQueen - index >= 0 && jQueen - index >= 0) {
-                if (boardStart[iQueen - index][jQueen - index] == 1) {
-                    return true;
-                }
-            }
+        if (boardStateXL[iQueen + jQueen] == 1) {
+            return true;
         }
         return false;
     }
 
-    private int[][] updateAttacked(int iQueen, int jQueen, int[][] attacked) {
-        int[][] newAttacked = cloneArray(attacked);
-        for (int i = 0; i < BOARDSIZE; i++) {
-            newAttacked[i][jQueen] = 1;
-        }
-        for (int j = 0; j < BOARDSIZE; j++) {
-            newAttacked[iQueen][j] = 1;
-        }
-        for (int index = 0; index < BOARDSIZE; index++) {
-            if (iQueen + index < BOARDSIZE && jQueen + index < BOARDSIZE) {
-                newAttacked[iQueen + index][jQueen + index] = 1;
-            }
-            if (iQueen - index >= 0 && jQueen + index < BOARDSIZE) {
-                newAttacked[iQueen - index][jQueen + index] = 1;
-            }
-            if (iQueen + index < BOARDSIZE && jQueen - index >= 0) {
-                newAttacked[iQueen + index][jQueen - index] = 1;
-            }
-            if (iQueen - index >= 0 && jQueen - index >= 0) {
-                newAttacked[iQueen - index][jQueen - index] = 1;
-            }
-        }
-        return newAttacked;
-    }
-
-    private int[][] cloneArray(int[][] src) {
-        int length = src.length;
-        int[][] target = new int[length][src[0].length];
-        for (int i = 0; i < length; i++) {
-            System.arraycopy(src[i], 0, target[i], 0, src[i].length);
-        }
-        return target;
-    }
 }
